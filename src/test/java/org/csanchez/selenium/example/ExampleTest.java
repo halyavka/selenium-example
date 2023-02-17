fa4574af3a32301b39ff600ab748562c45838a6d
halyavka <vanle@ukr.net>
2023-02-17 15:32:43 +0200

    Update ExampleTest.java

--- src/test/java/org/csanchez/selenium/example/ExampleTest.java
+++ src/test/java/org/csanchez/selenium/example/ExampleTest.java
@@ -19,7 +19,6 @@ public class ExampleTest {
 
     private final static String SELENIUM_URL = System.getProperty("selenium.url", "http://localhost:4444/wd/hub");
     private final static String SELENIUM_BROWSER = System.getProperty("selenium.browser", "chrome");
-    private final static int SLEEP = Integer.parseInt(System.getProperty("sleep", "10000"));
 
     protected WebDriver driver;
 
@@ -28,7 +27,7 @@ public class ExampleTest {
         DesiredCapabilities capabilities = new DesiredCapabilities(SELENIUM_BROWSER, "", Platform.ANY);
         // Retry connecting
         WebDriverException ex = null;
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             try {
                 this.driver = new RemoteWebDriver(new URL(SELENIUM_URL), capabilities);
                 return;
@@ -48,33 +47,8 @@ public class ExampleTest {
         // Alternatively the same thing can be done like this
         // driver.navigate().to("http://www.google.com");
 
-        Thread.sleep(SLEEP);
+        Thread.sleep(3000);
 
-        // Find the text input element by its name
-        WebElement element = driver.findElement(By.name("q"));
-
-        // Enter something to search for
-        element.sendKeys("Cheese!");
-
-        Thread.sleep(SLEEP);
-
-        // Now submit the form. WebDriver will find the form for us from the element
-        element.submit();
-
-        // Check the title of the page
-        System.out.println("Page title is: " + driver.getTitle());
-
-        // Google's search is rendered dynamically with JavaScript.
-        // Wait for the page to load, timeout after 10 seconds
-        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
-            public Boolean apply(WebDriver d) {
-                return d.getTitle().toLowerCase().startsWith("cheese!");
-            }
-        });
-
-        Thread.sleep(SLEEP);
-
-        // Should see: "cheese! - Google Search"
         System.out.println("Page title is: " + driver.getTitle());
 
     }
